db967a574eb306b7e94f2c5b0f59a313fe72f4d1
Fabio Scagliola <scagliola@gmail.com>
2025-05-18 11:19:35 +0200

    Reacting

--- NerdyWeirdWords/app/routes/home.tsx
+++ NerdyWeirdWords/app/routes/home.tsx
@@ -1,13 +1,16 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
+import {Welcome} from "~/welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        {name: "author", content: "Fabio Scagliola"},
+        {name: "description", content: "A web app for writers to share their work with beta readers, gather feedback, and engage with them."},
+        {name: "keywords", content: ""},
+        {title: "/nerdy|weird|words/"},
+    ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <Welcome/>;
 }
+
