85de083deee6d693e294dbd09d72cd9d1095e7b7
Mirco Dascoli <mircodascoli3@gmail.com>
2025-11-25 12:37:24 +0100

    last little fixes

--- NerdyWeirdWords/app/routes/upload-writing.tsx
+++ NerdyWeirdWords/app/routes/upload-writing.tsx
@@ -92,23 +92,19 @@ export default function UploadWriting() {
                 <h1>Upload writing</h1>
                 <p>Pick a file, give it a title, optionally a description, and upload your writing.</p>
             </div>
-
             <form className="col-10 col-lg-5 mx-auto my-5" encType="multipart/form-data" noValidate onSubmit={handleClick}>
                 <div className="mb-3">
                     <label htmlFor="writing" className="form-label">Writing</label>
                     <input type="file" className="form-control" id="writing" name="writing"/>
                 </div>
-
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Title</label>
                     <input type="text" className="form-control" id="title" name="title"/>
                 </div>
-
                 <div className="mb-3">
                     <label htmlFor="description" className="form-label">Description</label>
                     <textarea className="form-control" id="description" name="description"/>
                 </div>
-
                 <button type="submit" className="btn btn-primary d-flex">Upload</button>
 
                 {errorMessage && (
