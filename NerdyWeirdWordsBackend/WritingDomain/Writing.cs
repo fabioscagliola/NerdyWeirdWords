50e0f68193f1e7ed563637f77143063756f05f53
Mirco Dascoli <mircodascoli3@gmail.com>
2026-01-19 13:47:05 +0100

    Refactor WritingDomain classes: remove default values and establish inheritance for Heading and Paragraph

--- NerdyWeirdWordsBackend/WritingDomain/Writing.cs
+++ NerdyWeirdWordsBackend/WritingDomain/Writing.cs
@@ -1,5 +1,10 @@
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
 namespace com.nerdyweirdwords.backend.WritingDomain;
 public class Writing
 {
- 
+ public Attribute[] Attributes { get; set; }
+  public Block[] Blocks { get; set; }
 }
