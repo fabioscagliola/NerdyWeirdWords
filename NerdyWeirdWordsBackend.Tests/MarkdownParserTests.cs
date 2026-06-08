27f335434fad61b0f9b3846b638995105b69a568
Mirco Dascoli <mircodascoli3@gmail.com>
2026-06-08 19:16:11 +0200

    Update project dependencies and add .gitignore for test project
    - Updated package references in NerdyWeirdWordsBackend.csproj by removing test-related packages.
    - Added new dependencies in project.nuget.cache for various packages.
    - Created a .gitignore file for NerdyWeirdWordsBackend.Tests to exclude unnecessary files and directories.

--- NerdyWeirdWordsBackend.Tests/MarkdownParserTests.cs
+++ NerdyWeirdWordsBackend.Tests/MarkdownParserTests.cs
@@ -1,15 +1,56 @@
-﻿namespace NerdyWeirdWordsBackend.Tests;
+﻿using com.nerdyweirdwords.backend.WritingDomain;
+using NUnit.Framework;
 
-public class Tests
+namespace NerdyWeirdWordsBackend.Tests;
+
+[TestFixture]
+public class MarkdownParserTests
 {
-    [SetUp]
-    public void Setup()
+    private const string Document = """
+        # The quick brown fox jumps over the lazy dog
+
+        *The quick brown fox jumps over the lazy dog* is an English-language pangram.
+
+        A **pangram** is a sentence that contains all the letters of the alphabet.
+
+        """;
+
+    [Test]
+    public void Parse_SampleDocument_ReturnsCorrectNumberOfBlocks()
     {
+        var result = MarkdownParser.Parse(Document);
+
+        Assert.That(result.Blocks, Has.Length.EqualTo(3));
     }
 
     [Test]
-    public void Test1()
+    public void Parse_SampleDocument_FirstBlockIsHeading()
     {
-        Assert.Pass();
+        var result = MarkdownParser.Parse(Document);
+
+        var heading = result.Blocks[0] as Heading;
+        Assert.That(heading, Is.Not.Null);
+        Assert.That(heading!.Level, Is.EqualTo(1));
+        Assert.That(heading.Text, Is.EqualTo("The quick brown fox jumps over the lazy dog"));
+    }
+
+    [Test]
+    public void Parse_SampleDocument_SecondBlockIsParagraph()
+    {
+        var result = MarkdownParser.Parse(Document);
+
+        var paragraph = result.Blocks[1] as Paragraph;
+        Assert.That(paragraph, Is.Not.Null);
+        Assert.That(paragraph!.Text, Is.EqualTo("*The quick brown fox jumps over the lazy dog* is an English-language pangram."));
+    }
+
+    [Test]
+    public void Parse_SampleDocument_ThirdBlockIsParagraph()
+    {
+        var result = MarkdownParser.Parse(Document);
+
+        var paragraph = result.Blocks[2] as Paragraph;
+        Assert.That(paragraph, Is.Not.Null);
+        Assert.That(paragraph!.Text, Is.EqualTo("A **pangram** is a sentence that contains all the letters of the alphabet."));
     }
 }
